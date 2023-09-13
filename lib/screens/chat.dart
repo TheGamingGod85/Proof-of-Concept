0b316a656f71b649f85ea0c8c73fc3afe0c5c555
Aayushya Lakkadwala <72059036+TheGamingGod85@users.noreply.github.com>
2023-09-13 23:47:21 +0530

    yeee

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:poc/widgets/theme.dart';
 import 'package:chat_bubbles/chat_bubbles.dart';
 
-
 class ChatPage extends StatefulWidget {
   @override
   _ChatPageState createState() => _ChatPageState();
@@ -21,46 +20,45 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: Padding(
         padding: EdgeInsets.only(top: 25),
-        child: Stack(
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-                      return BubbleNormal(
-                        text: messages[index].text,
-                        isSender: messages[index].isSender,
-                        color: Color(0xFF1B97F3),
-                        tail: true,
-                        textStyle: TextStyle(
-                          fontSize: 20,
-                          color: Colors.white,
-                        ),
-                      );
-                    },
-                  ),
-                  SizedBox(
-                    height: 100,
-                  )
-                ],
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: BubbleNormal(
+                      text: messages[index].text,
+                      isSender: messages[index].isSender,
+                      color: MyColor.crimson,
+                      tail: true,
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                },
               ),
             ),
-            MessageBar(
-              messageBarColor: MyColor.black,
-              sendButtonColor: MyColor.crimson,
-              onSend: (message) {
-                setState(() {
-                  messages.add(Message(text: message, isSender: true));
-                  // Simulate a reply from the other person
-                  messages.add(Message(
-                    text: 'This is a reply from the other person.',
-                    isSender: false,
-                  ));
-                });
-              },
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: MessageBar(
+                messageBarColor: MyColor.black,
+                sendButtonColor: MyColor.crimson,
+                onSend: (message) {
+                  setState(() {
+                    messages.add(Message(text: message, isSender: true));
+                    // Simulate a reply from the other person
+                    messages.add(Message(
+                      text: 'This is a reply from the other person.',
+                      isSender: false,
+                    ));
+                  });
+                },
+              ),
             ),
           ],
         ),
